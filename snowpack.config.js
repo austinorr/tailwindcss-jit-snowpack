f1f205f4801bae1587f151221f0a269ad01d1941
Xeevis <Xeevis@users.noreply.github.com>
2021-03-22 10:07:13 +0100

    Use postcss-cli

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,5 +2,13 @@ module.exports = {
   mount: {
     app: "/",
   },
-  plugins: ["@snowpack/plugin-postcss"],
+  plugins: [
+    [
+      "@snowpack/plugin-run-script",
+      {
+        cmd: "postcss ./app/app.css -o ./build/app.pcss --no-source-map", // production build command
+        watch: "postcss --watch  ./app/app.pcss -o ./build/app.css --no-source-map", // (optional) dev server command
+      },
+    ],
+  ],
 };
